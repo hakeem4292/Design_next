7b7b541f3567ca107cafeb22775a091c72848a40
hakeem4292 <hakeemummar@gmail.com>
2026-02-03 09:52:13 +0530

    config set

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    output: "export",     // 👈 required for static export
+  images: {
+    unoptimized: true,  // 👈 required for static hosting
+  },
 };
 
 export default nextConfig;
